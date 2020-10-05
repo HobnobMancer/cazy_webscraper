40f7e4cad9c05610f042453831818bc338f709b6
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-10-05 14:24:02 +0100

    create empty script to store webscraper

---
+++ scraper/cazy_scraper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+"""
+Web scraper to scrape CAZy website and retrieve all protein data.
+
+:cmd_args:...
+
+:func ...:...
+
+Produces a dataframe containing protein data and write protein sequences to FASTA files.
+"""
+
+import pandas as pd
+
