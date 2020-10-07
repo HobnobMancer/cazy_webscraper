a4b18ccffc69d0a04b3c758ac50dc039be2a81b0
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-10-07 10:17:08 +0100

    import logger and args parser into scraper

--- scraper/cazy_webscraper.py
+++ scraper/cazy_webscraper.py
@@ -27,19 +27,45 @@ Produces a dataframe containing protein data and write protein
 sequences to FASTA files.
 """
 
+import logging
 import re
 
-import pandas as pd
-
-from urllib.request import urlopen
+from typing import List, Optional
 
 import mechanicalsoup
 
+from scraper.utilities import build_parser, build_logger
 
-def main():
+# add in option to configure:
+# specify which classes to scrape
+# specify if subfamilies wanted or not
+
+# should I rename this directory crawler as this only crawls through the pages
+
+
+def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = None):
     """Coordinate retrieval of links to CAZy website pages, 
     required for retrieval of protein data.
+
+    :param argv: (optional) args_parser.Namespace object
     """
+    # Program preparation
+
+    # Build args parser
+    # Check if Namespace is passed, if not parse cmd-line
+    if argv is None:
+        # parse cmd-line
+        parser = build_parser()
+        args = parser.parse_args()
+    else:
+        args = build_parser(argv).parse_args()
+
+    # Build logger
+    # Log file only created if specified at cmd-line
+    if logger is None:
+        logger = build_logger("cazy_webscraper", args)
+    logger.info("Run initated")
+
     # page to start browser at: the CAZy homepage
     base_url = "http://www.cazy.org"
 
@@ -47,7 +73,7 @@ def main():
     browser = mechanicalsoup.Browser()
 
     # Retrieve all links from the CAZy homepage
-    links_dict = get_links(browser, base_url)
+    links_dict = get_all_homepage_links(browser, base_url)
 
     # tuple of CAZy classes and abbrievations
     cazy_classes = [
@@ -59,16 +85,31 @@ def main():
         ["Carbohydrate-Binding Modules", "CBM"],
     ]
 
-    # Navigate through each CAZy class pages
-    # iterating through all pages listing all CAZymes for each CAZy class
+    # Navigate through each CAZy class main page
     index = 0
 
-    for index in range(len(cazy_classes)):
+    for index in range(len(cazy_classes)):  # for each specificed class
+        # compile URL for the class main page
         class_url = base_url + "/" + links_dict[cazy_classes[index][0]]
-        family_links = get_family_links(browser, class_url, cazy_classes[1])
+        # retrieve URLs for each family's main page
+        family_links = get_family_links(browser, class_url, cazy_classes[1], args)
+
+        for family in family_links:
+            family_url = base_url + "/" + family
+            # get the link to the page for 'all' proteins catalogued in the family
+            protein_table_links = get_family_table_links(browser, family_url, base_url)
+            # parse CAZy protein tables
+
+    # call functions from pyrewton.cazymes.prediction.parse submodule
+
+    class_url = "www.cazy.org/Glycoside-Hydrolases.html"
+    family_links = get_family_links(browser, class_url, "GH")
+
+    family_url = base_url + "/" + family_links[0]
+    protein_table_links = get_family_table_links(browser, family_url, base_url)
 
 
-def get_links(browser, base_url):
+def get_all_homepage_links(browser, base_url):
     """Retrieve all links from the homepage.
 
     Return dictionary of link.text : link key/value pairs.
@@ -91,7 +132,7 @@ def get_links(browser, base_url):
     return link_dict
 
 
-def get_family_links(browser, class_url, class_abbreviation):
+def get_family_links(browser, class_url, class_abbreviation, args):
     """Navigate CAZy class page, iterating through pages listing CAZymes.
     
     Return list of links to CAZy family pages for given class.
@@ -103,9 +144,7 @@ def get_family_links(browser, class_url, class_abbreviation):
 
     # empty lists to store links to family pages
     family_links = []
-    sub_family_links = []
-    # empty list to store the names of families with subfamilies
-    sub_family_numbers = []
+
     # search pattern to determine if link is for CAZy family or not
     pattern = re.compile(rf"{class_abbreviation}\d+?.*?\.html")
 
@@ -113,46 +152,101 @@ def get_family_links(browser, class_url, class_abbreviation):
     for link in all_links:
         # retrieve the link from the bs4 object
         try:
+            # check link is for a family/subfamily page
             address = link["href"]
+            search_result = re.match(pattern, link.text)
+            if search_result is True:  # link is a CAZy family/subfamily page link
+
+                if args.subfamily is False:  # do not retrieve subfamily links
+                    # subfamily names contain '_'
+                    subfam_index = address.find("_")
+                    if subfam_index != -1:  # link is for family page
+                        family_links.append(address)
+
+                else:  # retrieve all family and subfamily page links
+                    family_links.append(address)
+                    # duplicate protein results caused by retrieving family and
+                    # subfamily data are removed when parsing the pages
+                    # becuase some proteins catalogued under the family with
+                    # no subfamily
         except KeyError:
             pass
 
-        # check if link is for CAZy family page
-        search_result = re.match(pattern, address)
-
-        if search_result:  # link is a CAZy family page link
-            # check if subfamily (subfamilies containing '_' in their name)
-            subfam_index = address.find("_")
-
-            if subfam_index == -1:
-                # if link is for a subfamily page
-                family_links.append(address)
-
-            else:
-                # if link is for a subfamily page
-                # retrieve number of the main family
-                # e.g. retrieve 'GH13' from 'GH13_1.html'
-                family_number = address[:subfam_index]
-                sub_family_numbers.append(family_number)
-                sub_family_links.append(address)
-
-    # Remove the links to families which appear in the subfamilies
-    # Otherwise would collect proteins within the subfamilies twice,
-    # once in when going through the family pages, and second when
-    # going through the subfamily pages
-
-    # remove duplicates
-    sub_family_numbers = list(dict.fromkeys(sub_family_numbers))
-
-    for number in sub_family_numbers:
-        for link in family_links:
-            if link.startswith("link"):
-                family_links.remove(link)
-
-    # combine the sub_family and family lists into a single list
-    family_links += sub_family_links
-
     return family_links
+
+
+def get_family_table_links(browser, family_url, base_url):
+    """Retrieves the links for all tables containing proteins for the family.
+
+    :param browser: Beautifulsoup browser objecct
+    :param family_link: Beautifulsoup link object
+
+    Return a list of urls for all pages containing protein data.
+    """
+    family_page = browser.get(family_url)
+
+    # obtain all links on family main/summary page
+    all_links = family_page.soup.select("a")
+
+    # empty dictionary to store links:
+    all_links_dict = {}
+
+    for link in all_links:
+        try:
+            all_links_dict[link.text] = link["href"]
+        except KeyError:
+            pass
+
+    # retrieves full url not only the suffix
+    family_all_url = all_links_dict["All"]
+
+    # navigate to family 'all' page
+    family_all_page = browser.get(family_all_url)
+
+    # obtain all links on family 'all' page
+    all_links = family_all_page.soup.select("a")
+
+    all_link_dict = {}
+    for link in all_links:
+        try:
+            all_link_dict[link.text] = link["href"]
+        except KeyError:
+            pass
+
+    # retrieve all links to the pages containing protein tables/data
+    # for the family
+    table_pages_urls = []
+    table_pages_urls.append(family_all_url)  # contains first table
+
+    # iterate through the page numbers to retrieve the links
+    # of all the pages containing protein tables for the family
+    # These cannot be retrieved in on go as each page contains
+    # a limited number of links to the other table pages.
+    page_count = 2
+    error_check = 0
+    while error_check == 0:
+        try:
+            new_url = base_url + "/" + all_link_dict[str(page_count)]
+            table_pages_urls.append(new_url)
+
+            # open new url to retrieve next page and
+            # retrieve all links from next page
+            # so can repeat and retrieve link for next page
+            new_page = browser.get(new_url)
+            all_links = new_page.soup.select("a")
+
+            all_link_dict = {}
+            for link in all_links:
+                try:
+                    all_link_dict[link.text] = link["href"]
+                except KeyError:
+                    pass
+
+            page_count += 1
+
+        except KeyError as error:
+            error_check = -1
+    return table_pages_urls
 
 
 if __name__ == "__main__":
