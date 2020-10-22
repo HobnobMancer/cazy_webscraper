ac7fd01c36ecd7818172e52ca8961bdec830d976
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-10-22 11:23:07 +0100

    create script for unit tests

---
+++ tests/test_parse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+"""Contains unit tests to testing scrape.parse submodule.
+
+These tests are inteded to be run from the root repository using:
+pytest -v
+"""
+
+import pytest
+
+import pandas as pd
+
+from argparse import Namespace, ArgumentParser
+
+from scraper import parse
+
+
+# define pytest fixtures for testing scraper.parse
+
+
+# units test for testing scraper.parse
+
