691b1a115c3242426e5c6945dfeb550a7f59a375
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-11-12 14:32:11 +0000

    create empty script to add pytests that test cazy_webscraper.py

---
+++ tests/test_webscraper.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+
+"""Tests the script cazy_webscraper.py which crawls the CAZy database and retrieves protein data.
+
+These test are intened to be run from the root of the repository using:
+pytest -v
+
+"""
+
+import pytest
