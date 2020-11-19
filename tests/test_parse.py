b331cec76b70212456b0337bf147f60a3b70c6f3
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-11-19 15:45:29 +0000

    add tests that test the parsing of proteins to a dataframe

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -16,23 +16,111 @@
 # UK
 
 # The MIT License
-"""Contains unit tests to testing scrape.parse submodule.
 
-These tests are inteded to be run from the root repository using:
+"""Tests the module parse which parses the protein data to produce a dataframe.
+
+These test are intened to be run from the root of the repository using:
 pytest -v
+
 """
 
 import pytest
 
-import pandas as pd
+from argparse import Namespace
 
-from argparse import Namespace, ArgumentParser
-
-from scraper import parse
+from scraper import cazy_webscraper, parse
 
 
-# define pytest fixtures for testing scraper.parse
+@pytest.fixture
+def family():
+    family = cazy_webscraper.Family("GH1_test", "GHtest")
+
+    protein = cazy_webscraper.Protein("protein_name", "GH1", "1.2.3.4", "organism")
+    family.members.add(protein)
+
+    protein = None
+    family.members.add(protein)
+
+    protein = cazy_webscraper.Protein(
+        "protein",
+        "GH1",
+        "",
+        "organism",
+        {"GenBank": ["link1"], "UniProt": ["link2"], "PDB": ["link3"]},
+    )
+    family.members.add(protein)
+
+    return family
 
 
-# units test for testing scraper.parse
+@pytest.fixture
+def args_ds_fam_no_subfam(test_dir):
+    args_dict = {
+        "args": Namespace(
+            subfamilies=False,
+            data_split="family",
+            output=test_dir,
+            force=False,
+        )
+    }
+    return args_dict
 
+
+@pytest.fixture
+def args_ds_class_subfam(test_dir):
+    args_dict = {
+        "args": Namespace(
+            subfamilies=True,
+            data_split="class",
+            output=test_dir,
+            force=False,
+        )
+    }
+    return args_dict
+
+
+@pytest.fixture
+def args_ds_none(test_dir):
+    args_dict = {
+        "args": Namespace(
+            subfamilies=False,
+            data_split=None,
+            output=test_dir,
+            force=False,
+        )
+    }
+    return args_dict
+
+
+def test_parse_ds_fam_no_subfams(args_ds_fam_no_subfam, family, null_logger, monkeypatch):
+    """Test proteins_to_dataframe when data split is family and subfamilies is False."""
+
+    def mock_write_df(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(parse, "write_out_df", mock_write_df)
+
+    parse.proteins_to_dataframe([family], args_ds_fam_no_subfam["args"], null_logger)
+
+
+
+def test_parse_ds_class_subfams(args_ds_class_subfam, family, null_logger, monkeypatch):
+    """Test proteins_to_dataframe when data split is class and subfamilies is True."""
+
+    def mock_write_df(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(parse, "write_out_df", mock_write_df)
+
+    parse.proteins_to_dataframe([family], args_ds_class_subfam["args"], null_logger)
+
+
+def test_parse_ds_none(args_ds_none, family, null_logger, monkeypatch):
+    """Test proteins_to_dataframe when data split is None."""
+
+    def mock_write_df(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(parse, "write_out_df", mock_write_df)
+
+    parse.proteins_to_dataframe([family], args_ds_none["args"], null_logger)
