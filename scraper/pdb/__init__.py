efd840c74441ea5252f2493dc5bacd8f8c9d9bfa
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-11-20 16:40:51 +0000

    create empty modules to add nessary functions into

---
+++ scraper/pdb/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+"""Module for retrieving protein structures from PDB."""
