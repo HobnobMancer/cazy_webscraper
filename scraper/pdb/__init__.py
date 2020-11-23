339b807fb51f9bd9891e7cc3181041a1653e2439
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-11-23 20:34:36 +0000

    add pymol commands to load and save strucctures

--- scraper/pdb/__init__.py
+++ scraper/pdb/__init__.py
@@ -17,3 +17,19 @@
 
 # The MIT License
 """Module for retrieving protein structures from PDB."""
+
+from bioservices import PDB
+
+from pymol import cmd
+
+
+accession = "test"
+
+# user will specify the file format at at the cmd when enabling pdb
+
+# put in try execpt in case error is raised because cant connect
+structure_file = PDB.get_file(identifier=accession, frmt=args.pdb)
+cmd.load(accession, object=structure_file)
+cmd.save(accession)
+
+# need to find out how to use pymol to save the structure file
