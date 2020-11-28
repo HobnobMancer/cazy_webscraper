c2525d6a3f2a98facc9d4942a5d66ddc4e2ba8a5
HobnobMancer <eemh1@st-andrews.ac.uk>
2020-11-28 15:10:41 +0000

    add pdb/genbank to Namespace and mock pdb/genbank retrieving functions

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -32,10 +32,23 @@ from scraper import cazy_webscraper, parse
 
 
 @pytest.fixture
+def out_dir(test_dir):
+    path = test_dir / "test_outputs" / "test_outputs_file_io"
+    return path
+
+
+@pytest.fixture
 def family():
     family = cazy_webscraper.Family("GH1_test", "GHtest")
 
-    protein = cazy_webscraper.Protein("protein_name", "GH1", "1.2.3.4", "organism")
+    protein = cazy_webscraper.Protein(
+        "protein_name",
+        "GH1",
+        "1.2.3.4",
+        "organism",
+        {"GenBank": ["link1"], "UniProt": ["link2"], "PDB/3D": ["link3"]},
+        {"link1": ["A", "B", "C"]},
+    )
     family.members.add(protein)
 
     protein = None
@@ -61,6 +74,10 @@ def args_ds_fam_no_subfam(test_dir):
             data_split="family",
             output=test_dir,
             force=False,
+            genbank="dummy_email",
+            genbank_output=test_dir,
+            pdb="pdb",
+            pdb_output=test_dir,
         )
     }
     return args_dict
@@ -74,6 +91,10 @@ def args_ds_class_subfam(test_dir):
             data_split="class",
             output=test_dir,
             force=False,
+            genbank="dummy_email",
+            genbank_output=test_dir,
+            pdb=None,
+            pdb_output=None,
         )
     }
     return args_dict
@@ -87,30 +108,35 @@ def args_ds_none(test_dir):
             data_split=None,
             output=test_dir,
             force=False,
+            genbank=None,
+            genbank_output=None,
+            pdb="pdb",
+            pdb_output=test_dir,
         )
     }
     return args_dict
 
 
-def test_parse_ds_fam_no_subfams(args_ds_fam_no_subfam, family, null_logger, monkeypatch):
+def test_prt_to_df_ds_fam_no_subfams(args_ds_fam_no_subfam, family, null_logger, monkeypatch):
     """Test proteins_to_dataframe when data split is family and subfamilies is False."""
 
-    def mock_write_df(*args, **kwargs):
+    def mock_no_return(*args, **kwargs):
         return
 
-    monkeypatch.setattr(parse, "write_out_df", mock_write_df)
+    monkeypatch.setattr(parse, "write_out_df", mock_no_return)
+    monkeypatch.setattr(parse, "get_structures_and_sequences", mock_no_return)
 
     parse.proteins_to_dataframe([family], args_ds_fam_no_subfam["args"], null_logger)
-
 
 
 def test_parse_ds_class_subfams(args_ds_class_subfam, family, null_logger, monkeypatch):
     """Test proteins_to_dataframe when data split is class and subfamilies is True."""
 
-    def mock_write_df(*args, **kwargs):
+    def mock_no_return(*args, **kwargs):
         return
 
-    monkeypatch.setattr(parse, "write_out_df", mock_write_df)
+    monkeypatch.setattr(parse, "write_out_df", mock_no_return)
+    monkeypatch.setattr(parse, "get_genbank_fasta", mock_no_return)
 
     parse.proteins_to_dataframe([family], args_ds_class_subfam["args"], null_logger)
 
@@ -118,9 +144,10 @@ def test_parse_ds_class_subfams(args_ds_class_subfam, family, null_logger, monke
 def test_parse_ds_none(args_ds_none, family, null_logger, monkeypatch):
     """Test proteins_to_dataframe when data split is None."""
 
-    def mock_write_df(*args, **kwargs):
+    def mock_no_return(*args, **kwargs):
         return
 
-    monkeypatch.setattr(parse, "write_out_df", mock_write_df)
+    monkeypatch.setattr(parse, "write_out_df", mock_no_return)
+    monkeypatch.setattr(parse, "get_pdb_structures", mock_no_return)
 
     parse.proteins_to_dataframe([family], args_ds_none["args"], null_logger)
