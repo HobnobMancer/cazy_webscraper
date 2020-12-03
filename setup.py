7196a495353443ded155cc7a8722b4685f0cfbc1
Leighton Pritchard <leighton.pritchard@mac.com>
2020-12-03 15:17:53 +0000

    fix typo (missing comma)

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
     platforms="Posix, MacOS X",
     url="https://github.com/HobnobMancer/cazy_webscraper",
     entry_points={"console_scripts": ["cazy_webscraper.py = scraper.cazy_webscraper:main"]},
-    install_requires=["biopython>=1.76", "pandas>=1.0.3", "requests", "mechanicalsoup", "tqdm", "pyyaml"]
+    install_requires=["biopython>=1.76", "pandas>=1.0.3", "requests", "mechanicalsoup", "tqdm", "pyyaml"],
     packages=setuptools.find_packages(),
     package_data={
         "Conda microenvironment": ["environment.yml"],
