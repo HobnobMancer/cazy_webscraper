c8507196c5e6d5bc9c83a5e1b6f5a0fb1da3fb65
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-01-25 17:53:16 +0000

    create module for expanding the database beyond what is directly stored in CAZy.

---
+++ scraper/expand/genbank_sequences.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+"""Retrieve proteins sequences from GenBank and populate the local database and write to FASTA"""
