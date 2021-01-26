13ed103cd0c9df9f11cc6e8e7ab8cdc528bc4d51
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-01-26 17:06:47 +0000

    create args parser for pdb_structures

--- scraper/utilities/__init__.py
+++ scraper/utilities/__init__.py
@@ -268,16 +268,6 @@ def build_genbank_sequences_parser(argv: Optional[List] = None):
         help="Families to scrape. Separate families by commas 'GH1,GH2'"
     )
 
-    # Add option to download FASTA file for protein from GenBank
-    parser.add_argument(
-        "-g",
-        "--genbank",
-        type=str,
-        metavar="Email address of user",
-        default=None,
-        help="Enable FASTA files from GenBank, and user email required for Entrez",
-    )
-
     # Add log file name option
     # If not given, no log file will be written out
     parser.add_argument(
@@ -319,6 +309,108 @@ def build_genbank_sequences_parser(argv: Optional[List] = None):
         metavar="path to FASTA file dire",
         default=None,
         help="Enable writing out protein sequences to a FASTA file",
+    )
+
+    # Add option for more detail (verbose) logging
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=False,
+        help="Set logger level to 'INFO'",
+    )
+
+    if argv is None:
+        # parse command-line
+        return parser
+    else:
+        # return namespace
+        return parser.parse_args(argv)
+
+
+def build_pdb_structures_parser(argv: Optional[List] = None):
+    """Return ArgumentParser parser for the script 'expand.genbank_sequences.py'."""
+    # Create parser object
+    parser = argparse.ArgumentParser(
+        prog="pdb_structures.py",
+        description="Download structures from PDB",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    # Add positional/required arguments
+    parser.add_argument(
+        "database",
+        type=Path,
+        metavar="local CAZy database",
+        help="Path to local CAZy database",
+    )
+
+    parser.add_argument(
+        "pdb",
+        choices=["mmCif", "pdb", "xml", "mmtf", "bundle"],
+        type=str,
+        help="File format of downloaded structure from PDB",
+    )
+
+    parser.add_argument(
+        "outdir",
+        type=Path,
+        metavar="output directory path",
+        help="Path to output directory to which downloaded structures are retrieved",
+    )
+
+    # Add optional arguments to parser
+
+    # Add option to define classes to retrieve protein sequences for
+    parser.add_argument(
+        "-c",
+        "--classes",
+        type=str,
+        default=None,
+        help="Classes from which all families are to be scraped. Separate classes by ','"
+    )
+
+    # Add option to specify families to retrieve protein sequences for
+    parser.add_argument(
+        "-f",
+        "--families",
+        type=str,
+        default=None,
+        help="Families to scrape. Separate families by commas 'GH1,GH2'"
+    )
+
+    # Add log file name option
+    # If not given, no log file will be written out
+    parser.add_argument(
+        "-l",
+        "--log",
+        type=Path,
+        metavar="log file name",
+        default=None,
+        help="Defines log file name and/or path",
+    )
+
+    # enable retrieving protein sequences for only primary GenBank accessions
+    parser.add_argument(
+        "-p",
+        "--primary",
+        dest="primary",
+        action="store_true",
+        default=False,
+        help="Enable retrieveing protein sequences for only primary GenBank accessions",
+    )
+
+    # Add option to update sequences if the retrieved sequence is different
+    # If not enabled then sequences will only be retrieved and added for proteins that do not
+    # already have a protein sequence
+    parser.add_argument(
+        "-u",
+        "--update",
+        dest="update",
+        action="store_true",
+        default=False,
+        help="Enable overwriting sequences in the database if the retrieved sequence is different",
     )
 
     # Add option for more detail (verbose) logging
