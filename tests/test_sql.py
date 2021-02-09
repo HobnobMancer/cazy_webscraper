be2f995f48a65b4f6a55753f155e291ef6d061e8
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-02-09 19:50:17 +0000

    add tests for handling when multiple identical primary genbank accessions are found in the local db

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -84,12 +84,13 @@ def test_regex_search(db_session):
 
 def test_adding_a_new_protein(db_session):
     """Test adding a new protein to the local database."""
+    time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     sql_interface.add_protein_to_db(
         "test_cazyme",
         "test_fam",
         "test_genus test_species",
-        "test_primary_genbank",
+        time_stamp,
         db_session,
         ec_numbers=["EC1.2.3.4"],
         genbank_accessions=["Genbank1", "Genbank2"],
@@ -110,4 +111,102 @@ def test_add_data_to_an_existing_record_in_db(db_session):
         ec_numbers=["EC4.2.2.-"],
         genbank_accessions=["Genbank1", "Genbank2"],
         uniprot_accessions=["Uni1", "Uni2"],
+    )
+
+
+def test_genbank_no_cazymes(db_session, monkeypatch):
+    """test adding a protein to a database, GenBank accession is found with no linked CAZymes."""
+
+    def mock_adding_a_new_protein(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(sql_interface, "add_new_protein_to_db", mock_adding_a_new_protein)
+
+    existing_genbank_with_no_cazyme = "test_genbank_no_cazyme"
+
+    sql_interface.add_protein_to_db(
+        "test_cazyme_name",
+        "cazy_family",
+        "source_genus organism",
+        existing_genbank_with_no_cazyme,
+        db_session,
+        ec_numbers=["EC4.2.2.-"],
+        genbank_accessions=["Genbank1", "Genbank2"],
+        uniprot_accessions=["Uni1", "Uni2"],
+    )
+
+
+def test_one_genbank_multiple_cazymes(db_session, monkeypatch):
+    """test adding protein to db when genbank is found linked to muliple CAZymes."""
+
+    def mock_add_protein_to_db(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(sql_interface, "add_data_to_protein_record", mock_add_protein_to_db)
+
+    genbank_with_multiple_cazymes = "one_genbank_multi_cazymes"
+
+    sql_interface.add_protein_to_db(
+        "test_cazyme_name",
+        "cazy_family",
+        "source_genus organism",
+        genbank_with_multiple_cazymes,
+        db_session,
+    )
+
+
+def test_multiple_genbanks_multiple_cazymes(db_session, monkeypatch):
+    """test adding protein to db when finding multiple identical CAZymes and GenBank accesisons."""
+
+    def mock_add_protein_to_db(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(sql_interface, "add_data_to_protein_record", mock_add_protein_to_db)
+
+    identical_genbank_accession = "identical_accession"
+
+    sql_interface.add_protein_to_db(
+        "test_cazyme_name",
+        "cazy_family",
+        "source_genus organism",
+        identical_genbank_accession,
+        db_session,
+    )
+
+
+def test_multiple_genbanks_one_cazyme(db_session, monkeypatch):
+    """test adding protien to db when identical GenBank accessions with one CAZyme link."""
+
+    def mock_add_protein_to_db(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(sql_interface, "add_data_to_protein_record", mock_add_protein_to_db)
+
+    multiple_accession = "multiple_accession"
+
+    sql_interface.add_protein_to_db(
+        "test_cazyme_name",
+        "cazy_family",
+        "source_genus organism",
+        multiple_accession,
+        db_session,
+    )
+
+
+def test_multiple_genbanks_no_cazymes(db_session, monkeypatch):
+    """test adding protien to db when identical GenBank accessions are linked to no CAZymes."""
+
+    identical_genbanks_no_cazymes = "multi_accession_no_caz"
+
+    def mock_add_protein_to_db(*args, **kwargs):
+        return
+
+    monkeypatch.setattr(sql_interface, "add_new_protein_to_db", mock_add_protein_to_db)
+
+    sql_interface.add_protein_to_db(
+        "test_cazyme_name",
+        "cazy_family",
+        "source_genus organism",
+        identical_genbanks_no_cazymes,
+        db_session,
     )
