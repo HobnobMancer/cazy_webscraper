7f238b419b749003a89367d6c6b3535dc81de37e
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-02-22 12:08:51 +0000

    change UniqueConstraint data type to a tuple

--- scraper/sql/sql_orm.py
+++ scraper/sql/sql_orm.py
@@ -313,7 +313,9 @@ class Cazymes_Genbanks(Base):
     by the GenBank accession because duplicate entries for CAZyme can be found within CAZy.
     """
     __tablename__ = "cazymes_genbanks"
-    __table_args__ = (UniqueConstraint("cazyme_id", "genbank_id"))
+    __table_args__ = (
+        UniqueConstraint("cazyme_id", "genbank_id"),
+    )
 
     link_id = Column(Integer, primary_key=True)  # unique ID of the CAZyme-GenBank relationship
     cazyme_id = Column(Integer, ForeignKey("cazymes.cazyme_id"))
