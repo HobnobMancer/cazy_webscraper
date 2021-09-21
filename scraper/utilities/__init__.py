3f10a87c5364b4412fb45dc95428595462bf0105
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-09-21 20:03:35 +0100

    explictly set logger level

--- scraper/utilities/__init__.py
+++ scraper/utilities/__init__.py
@@ -60,51 +60,54 @@ def config_logger(args) -> logging.Logger:
     Return nothing
     """
     logger = logging.getLogger(__package__)
-
-    # Set format of loglines
-    log_formatter = logging.Formatter("[%(levelname)s] [%(name)s]: %(message)s")
+    logger.setLevel(logging.INFO)
 
     # define logging level
-    if args.verbose is True:
+    if args.verbose:
         logger.setLevel(logging.INFO)
     else:
         logger.setLevel(logging.WARNING)
 
+    # Set format of loglines
+    log_formatter = logging.Formatter("[%(levelname)s] [%(name)s]: %(message)s")
+
     # Setup console handler to log to terminal
     console_log_handler = logging.StreamHandler()
     console_log_handler.setFormatter(log_formatter)
+    if args.verbose:
+        console_log_handler.setLevel(logging.INFO)
+    else:
+        console_log_handler.setLevel(logging.WARNING)
     logger.addHandler(console_log_handler)
 
     # Setup file handler to log to a file
     if args.log is not None:
         file_log_handler = logging.FileHandler(args.log)
         file_log_handler.setFormatter(log_formatter)
+        if args.verbose:
+            file_log_handler.setLevel(logging.INFO)
+        else:
+            file_log_handler.setLevel(logging.WARNING)
         logger.addHandler(file_log_handler)
 
     return
 
 
-def build_logger(output, file_name):
+def build_logger(output):
     """Build loggers with pre-defined parameters for writing out errors and failed scrapes.
 
     :param output: Path to output dir or None
-    :param file_name: str, name of output log file
 
     Return logger object.
     """
-    logger = logging.getLogger(file_name[:-4])
-
-    if output is None:
-        output = os.getcwd()
-        path_ = Path(f"{output}/{file_name}")
-    else:
-        path_ = output / f"{file_name}"
+    file_name = output.name.split('.')[0]
+    logger = logging.getLogger(file_name)
 
     # Set format of loglines
     log_formatter = logging.Formatter(file_name + ": {} - {}".format("%(asctime)s", "%(message)s"))
 
     # Setup file handler to log to a file
-    file_log_handler = logging.FileHandler(path_)
+    file_log_handler = logging.FileHandler(output)
     file_log_handler.setLevel(logging.WARNING)
     file_log_handler.setFormatter(log_formatter)
     logger.addHandler(file_log_handler)
