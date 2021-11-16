c44f111164fd727ea784831583b41f831b713a4d
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-11-16 14:13:40 +0000

    add parsing db tables into dicts for uniprot data

--- cazy_webscraper/sql/sql_interface/get_table_dicts.py
+++ cazy_webscraper/sql/sql_interface/get_table_dicts.py
@@ -41,7 +41,95 @@
 """Retrieve all objects from a db table and parse the data to build a dict, repr the current table state."""
 
 
-from cazy_webscraper.sql.sql_orm import CazyFamily, Genbank, Kingdom, Session, Taxonomy
+from tqdm import tqdm
+
+from cazy_webscraper.sql.sql_orm import (
+    CazyFamily,
+    Ec,
+    Genbank,
+    Kingdom,
+    Pdb,
+    Session,
+    Taxonomy,
+    Uniprot,
+)
+
+
+def get_ec_table_dict(connection):
+    """Create dict of objects present in the CazyFamilies table.
+    
+    :param connection: open sqlalchemy db engine connection
+    
+    Return dict {ec_number: ec_id}
+    """
+    with Session(bind=connection) as session:
+        db_ec_records = session.query(Ec).all()
+    
+    ec_table_dict = {}  # {ec_number: ec_id}
+    for record in tqdm(db_ec_records, desc="Retrieving existing EC# records"):
+        ec_table_dict[record.ec_number] = record.ec_id
+
+
+def get_ec_gbk_table_dict(connection):
+    """Load the Genbanks_Ecs table into memory and compile a dict.
+
+    The the result dict is keyed by EC IDS.
+    
+    The table contains the current Genbank and EC number relationships in 
+    the local CAZyme db.
+    
+    :param connection: open sqlalchemy connection to an SQLite db
+    
+    Return dict {ec_id: {gbk ids}}
+    """
+    with Session(bind=connection) as session:
+        all_gbk_ec_records = session.query(Genbank, Ec).\
+            join(Ec, Genbank.ecs).\
+            all()
+        
+    ec_gbk_table_dict = {}
+    
+    for record in all_gbk_ec_records:
+        genbank_id = record[0].genbank_id
+        ec_id = record[1].ec_id
+        
+        try:
+            ec_gbk_table_dict[ec_id].add(genbank_id)
+        except KeyError:
+            ec_gbk_table_dict[ec_id] = {genbank_id}
+    
+    return ec_gbk_table_dict
+
+
+def get_gbk_ec_table_dict(connection):
+    """Load the Genbanks_Ecs table into memory and compile a dict.
+
+    The the result dict is keyed by GENBANK IDS.
+    
+    The table contains the current Genbank and EC number relationships in 
+    the local CAZyme db.
+    
+    :param connection: open sqlalchemy connection to an SQLite db
+    
+    Return dict {gbk_id: {ec ids}}
+    """
+    with Session(bind=connection) as session:
+        all_gbk_ec_records = session.query(Genbank, Ec).\
+            join(Ec, Genbank.ecs).\
+            all()
+        
+    gbk_ec_table_dict = {}
+    
+    for record in all_gbk_ec_records:
+        genbank_id = record[0].genbank_id
+        ec_id = record[1].ec_id
+        
+        try:
+            gbk_ec_table_dict[genbank_id].add(ec_id)
+        except KeyError:
+            gbk_ec_table_dict[genbank_id] = {ec_id}
+    
+    return gbk_ec_table_dict
 
 
 def get_fams_table_dict(connection):
@@ -142,6 +230,29 @@ def get_kingdom_table_dict(connection):
     return kingdom_dict
 
 
+def get_pdb_table_dict(connection):
+    """Create dict of objects present in the Pdbs table.
+    
+    :param connection: open sqlalchemy db engine connection
+    
+    Return dict {gbk_id: {pdb accessions}}
+    """
+    with Session(bind=connection) as session:
+        db_pdb_records = session.query(Pdb).all()
+
+    pdb_table_dict = {}  # {gbk_id: {pdb accessions}}
+
+    for record in tqdm(db_pdb_records, desc="Loading existing PDB db records"):
+        gbk_id = record.genbank_id
+        try:
+            pdb_table_dict[gbk_id].add(record.pdb_accession)
+            
+        except KeyError:
+            pdb_table_dict[gbk_id] = {record.pdb_accession}
+
+    pdb_table_dict 
+
+
 def get_taxs_table_dict(connection):
     """Create dict of objects present in the Taxs table.
     
@@ -167,3 +278,25 @@ def get_taxs_table_dict(connection):
     
     return db_tax_dict
 
+
+def get_uniprot_table_dict(connection):
+    """Create dict of objects present in the Uniprots table.
+    
+    :param connection: open sqlalchemy db engine connection
+    
+    Return dict {acc: {name: str, gbk_id: int, seq: str, seq_date:str } }
+    """
+    with Session(bind=connection) as session:
+        db_uniprot_records = session.query(Uniprot).all()
+
+    uniprot_table_dict = {}  # {acc: {name: str, gbk_id: int, seq: str, seq_date:str } }
+
+    for record in tqdm(db_uniprot_records, desc="Retrieving existing UniProt records from db"):
+        uniprot_table_dict[record.uniprot_accession] = {
+            "name": record.uniprot_name,
+            "genbank_id": record.genbank_id,
+            "seq": record.sequence,
+            "seq_date": record.seq_update_date,
+        }
+    
+    return uniprot_table_dict
