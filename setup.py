7c78fd3c755bb4cb609d0fff10b10594fc4c8ebf
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-11-16 16:53:08 +0000

    update entry point names and paths

--- setup.py
+++ setup.py
@@ -74,10 +74,12 @@ setuptools.setup(
     url="https://github.com/HobnobMancer/cazy_webscraper",
     entry_points={
         "console_scripts": [
-            "cazy_webscraper = scraper.cazy_webscraper:main",
-            "cw_get_genbank_sequences = scraper.expand.get_genbank_sequences:main",
-            "cw_get_pdb_structures = scraper.expand.get_pdb_structures:main",
-            "ce_get_uniprot_data = scraper.expand.uniprot.get_uniprot_data:main",
+            "cazy_webscraper = cazy_webscraper.cazy_webscraper:main",
+            "cw_get_genbank_sequences = cazy_webscraper.expand.genbank.get_genbank_sequences:main",
+            "cw_get_uniprot_data = cazy_webscraper.expand.uniprot.get_uniprot_data:main",
+            "cw_extract_genbank_sequences = cazy_webscraper.expand.extract.extract_genbank_sequences:main",
+            "cw_extract_uniprot_sequences = cazy_webscraper.expand.extract.extract_uniprot_sequences:main",
+            "cw_get_pdb_structures = cazy_webscraper.expand.pdb.get_pdb_structures:main",
         ]
     },
     install_requires=[
