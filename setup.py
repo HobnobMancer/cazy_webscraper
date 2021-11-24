577026836a838c114fcf1beb3cbe8853242e7398
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-11-24 15:53:01 +0000

    rename entry point, shorten it for ease

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setuptools.setup(
     entry_points={
         "console_scripts": [
             "cazy_webscraper = cazy_webscraper.cazy_webscraper:main",
-            "cw_get_genbank_sequences = cazy_webscraper.expand.genbank.get_genbank_sequences:main",
+            "cw_get_genbank_seqs = cazy_webscraper.expand.genbank.get_genbank_sequences:main",
             "cw_get_uniprot_data = cazy_webscraper.expand.uniprot.get_uniprot_data:main",
             "cw_extract_sequences = cazy_webscraper.expand.extract.extract_sequences:main",
             "cw_get_pdb_structures = cazy_webscraper.expand.pdb.get_pdb_structures:main",
