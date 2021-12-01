9204be48f0553680b4f0caa82511776fdd78cd61
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-12-01 17:07:52 +0000

    add validation of selected file types

--- cazy_webscraper/api/cw_query_database.py
+++ cazy_webscraper/api/cw_query_database.py
@@ -40,6 +40,7 @@
 
 
 import logging
+import sys
 
 import pandas as pd
 
@@ -50,8 +51,10 @@ from saintBioutils.utilities.logger import config_logger
 from saintBioutils.utilities import file_io
 
 from cazy_webscraper import cazy_scraper
+from cazy_webscraper.sql.sql_interface import get_selected_gbks
 from cazy_webscraper.utilities.parsers import query_db_parser
 from cazy_webscraper.utilities.parse_configuration import get_expansion_configuration
+
 
 def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = None):
     """Set up parser, logger and coordinate overal scrapping of CAZy."""
@@ -80,6 +83,8 @@ def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = No
         cache_dir = cache_dir / "uniprot_data_retrieval"
         file_io.make_output_directory(cache_dir, args.force, args.nodelete_cache)
 
+    output_files = validate_file_types(args)
+
     (
         config_dict,
         class_filters,
@@ -88,6 +93,80 @@ def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = No
         taxonomy_filter_dict,
         ec_filters,
     ) = get_expansion_configuration(args)
+
+    output_dict = {}  # dict to store all output from interrogating the CAZyme db
+
+    # get the records of GenBank accessions matching the criteria of interest
+    gbk_dict = get_selected_gbks.get_genbank_accessions(
+        class_filters,
+        family_filters,
+        taxonomy_filter_dict,
+        kingdom_filters,
+        ec_filters,
+        connection,
+    )
+
+    query_data = get_query_data(gbk_dict)
+
+    write_output(query_data, args)
+
+
+def validate_file_types(args):
+    """Validate the output files types chosen by the user
+    
+    :param args: cmd-line args parser
+    
+    Return list of output file types
+    """
+    logger = logging.getLogger(__name__)
+
+    file_types = args.file_types.split(",")
+
+    accepted_file_types = ['csv', 'json']
+    
+    chosen_types = [ftype.lower() for ftype in file_types if ftype.lower() in accepted_file_types]
+    for ftype in file_types:
+        if ftype.lower() in accepted_file_types:
+            chosen_types.append(ftype.lower())
+        else:
+            logger.error(f'File type: {ftype} not supported by cazy_webscraper')
+
+    if len(chosen_types) == 0:
+        logger.error(
+            "No accepted file types provided. Please chose at least one of the following:\n"
+            "csv, json\n"
+            "Terminating program"
+        )
+        sys.exit(1)
+    
+    return chosen_types
+
+
+def get_query_data(gbk_dict, args):
+    # create dict to store all data retrieved from the databsae query
+    query_data = {}
+    
+    # add the GenBank accessions of interest to the query dict
+    for gbk_acc in gbk_dict:
+        query_data[gbk_acc] = {}
+
+    if args.cazy_class or args.cazy_family or args.cazy_subfamily:
+        # retrieve the CAZy family annotations from the local CAZyme database
+    
+    if args.genus or args.organism:
+        # retrieve the taxonomy data from the local CAZyme database
+
+    if args.ec:
+       # retrieve the ec numbers from the local CAZyme database 
+
+    if args.pdb:
+        # retrieve the PDB accessions from the local CAZyme database
+
+    if args.uniprot or args.seq_uniprot:
+        # retrieve the UniProt data from the local CAZyme database
+
+    if args.seq_genbank:
+        # retrieve GEnbank protein sequences from the local CAZyme database
 
 
 if __name__ == "__main__":
