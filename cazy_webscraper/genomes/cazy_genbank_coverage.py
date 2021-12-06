a4e193c577ef69a0cec1e6d811de2415c4dbaa25
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-12-06 22:51:50 +0000

    add entry point for getting genbank coverage by CAZy

--- cazy_webscraper/genomes/cazy_genbank_coverage.py
+++ cazy_webscraper/genomes/cazy_genbank_coverage.py
@@ -39,10 +39,16 @@
 """Explore the number of GenBank genomes annotated by CAZy."""
 
 
+import logging
+
 import pandas as pd
 
 from datetime import datetime
 from typing import List, Optional
+
+from Bio import Entrez
+from saintBioutils.utilities.logger import config_logger
+from tqdm import tqdm
 
 from cazy_webscraper import cazy_scraper
 
@@ -51,6 +57,28 @@ def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = No
     time_stamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # used in naming files
     start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # used in terminating message
     start_time = pd.to_datetime(start_time)
+
+    # Program preparation
+    if argv is None:
+        parser = genbank_parser.build_parser()
+        args = parser.parse_args()
+    else:
+        parser = genbank_parser.build_parser(argv)
+        args = parser.parse_args()
+
+    if logger is None:
+        logger = logging.getLogger(__name__)
+        config_logger(args)
+
+    Entrez.email = args.email
+
+    # connect to the local CAZyme database
+    connection, logger_name, cache_dir = cazy_scraper.connect_existing_db(args, time_stamp)
+
+    # check if need to build output dir
+    ???
+
+
 
     end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     end_time = pd.to_datetime(end_time)
@@ -76,6 +104,40 @@ def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = No
             f"Citation: {cazy_scraper.CITATION_INFO}"
         )
 
+
+def get_genomic_accessions():
+    """Retrieve genomic accessions for the protein accessions in the local db.
+    
+    :param connection: open sqlalchemy connection to a SQLite db
+    :param args: cmd-line args parser
+    
+    Return dict {kingdom: {genomic_acc: {'proteins': [p_acc], 'count':#ofProteins}}}
+    """
+    # load the Genbank and Kingdom records from the local CAZyme db
+    genbank_kingdom_records = []
+
+    # extract the GenBank accessions and Kingdoms from the records
+    genbank_kingdom_dict = {}  # kingdom: {genbank_accessions}
+    for record in genbank_kingdom_records:
+        kingdom = record[1].kingdom
+        gbk_acc = record[0].genbank_accession
+
+        try:
+            genbank_kingdom_dict[kingdom].add(gbk_acc)
+        except KeyError:
+            genbank_kingdom_dict[kingdom] = gbk_acc
+
+    genomic_accession_dict = {}  #  {kingdom: {genomic_acc: {'proteins': [p_acc], 'count':#ofProteins}}}
+    
+    for kingdom in tqdm(genbank_kingdom_dict, desc="Retrieving genomic accessions per kingdom"):
+        gbk_accessions = genbank_kingdom_dict[kingdom]
+
+        # break up the list into a series of smaller lists that can be batched querried
+        batch_queries = []
+
+        for batch_query in batch_queries:
+
+            # 
 
 if __name__ == "__main__":
     main()
