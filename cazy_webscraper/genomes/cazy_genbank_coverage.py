3dde7367f336ac0e10d11d146129a0f4994a0884
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-12-06 15:42:33 +0000

    add module for calc GenBank coverage by CAZy

---
+++ cazy_webscraper/genomes/cazy_genbank_coverage.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) University of St Andrews 2020-2021
+# (c) University of Strathclyde 2020-2021
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Explore the number of GenBank genomes annotated by CAZy."""
+
+
+import pandas as pd
+
+from datetime import datetime
+from typing import List, Optional
+
+from cazy_webscraper import cazy_scraper
+
+
+def main(argv: Optional[List[str]] = None, logger: Optional[logging.Logger] = None):
+    time_stamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # used in naming files
+    start_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # used in terminating message
+    start_time = pd.to_datetime(start_time)
+
+    end_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    end_time = pd.to_datetime(end_time)
+    total_time = end_time - start_time
+
+    if args.verbose:
+        logger.info(
+            "Finished getting data from UniProt\n"
+            f"Scrape initated at {start_time}\n"
+            f"Scrape finished at {end_time}\n"
+            f"Total run time: {total_time}"
+            f"Version: {cazy_scraper.VERSION_INFO}\n"
+            f"Citation: {cazy_scraper.CITATION_INFO}"
+        )
+    else:
+        print(
+            "=====================cazy_webscraper=====================\n"
+            "Finished getting data from UniProt\n"
+            f"Scrape initated at {start_time}\n"
+            f"Scrape finished at {end_time}\n"
+            f"Total run time: {total_time}"
+            f"Version: {cazy_scraper.VERSION_INFO}\n"
+            f"Citation: {cazy_scraper.CITATION_INFO}"
+        )
+
+
+if __name__ == "__main__":
+    main()
