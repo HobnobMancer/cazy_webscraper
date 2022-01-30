2a014fd4e275eed6e04b804b0416ad59c54e7e23
HobnobMancer <hobnobmancer@Paddington.localdomain>
2022-01-30 16:10:09 +0000

    update version number

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with Path("README.md").open("r") as long_description_handle:
 
 setuptools.setup(
     name="cazy_webscraper",
-    version="2.0.3",
+    version="2.0.4",
     # Metadata
     author="Emma E. M. Hobbs",
     author_email="eemh1@st-andrews.ac.uk",
