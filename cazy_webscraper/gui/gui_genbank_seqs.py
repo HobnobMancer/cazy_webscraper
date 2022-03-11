3dbb1d7d27e51b96b0aa86836f3d824d7da0d2ca
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-03-11 16:58:57 +0000

    update copyright

--- cazy_webscraper/gui/gui_genbank_seqs.py
+++ cazy_webscraper/gui/gui_genbank_seqs.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) University of St Andrews 2020-2021
-# (c) University of Strathclyde 2020-2021
+# (c) University of St Andrews 2022
+# (c) University of Strathclyde 2022
+# (c) James Hutton Institute 2022
 # Author:
 # Emma E. M. Hobbs
 
