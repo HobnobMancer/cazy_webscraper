14c7084b0e076a8279b1235bc9bfa183d5d3fbde
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-03-14 11:44:07 +0000

    remove unused args

--- cazy_webscraper/gui/gui_get_uniprot_data.py
+++ cazy_webscraper/gui/gui_get_uniprot_data.py
@@ -181,7 +181,7 @@ def main():
         type=str,
         default=None,
         help=(
-            "Classes from which all families will be scraped. "
+            "Classes to retrieve protein data for. "
             "Separate classes with a single comma ',', e.g. 'GH,GT,PL'"
         ),
     )
@@ -191,17 +191,7 @@ def main():
         metavar="CAZy (sub)families",
         type=str,
         default=None,
-        help="Families and subfamilies to scrape. Separate families with single commas 'GH1,GH2'. CAZy families are case sensitive"
-    )
-
-    class_group.add_argument(
-        "-s",
-        "--subfamilies",
-        metavar="Retrieve subfamilies",
-        dest="subfamilies",
-        action="store_true",
-        default=False,
-        help="Enable retrieving of subfamily annotations from CAZy",
+        help="Families and subfamilies to retrieve protein data for. Separate families with single commas 'GH1,GH3_1'. CAZy families are case sensitive"
     )
 
     #
