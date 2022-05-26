ed25f05368b10f79115d9b5546a857b21ac23116
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-05-26 10:09:13 +0100

    update mod desc

--- cazy_webscraper/ncbi/taxonomy/__init__.py
+++ cazy_webscraper/ncbi/taxonomy/__init__.py
@@ -36,4 +36,4 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-"""Module for interacting with the NCBI databases."""
+"""Module for interacting with the NCBI databases to retrieve taxonomy data."""
