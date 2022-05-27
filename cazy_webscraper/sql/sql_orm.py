37a4911fa0e22fe2d3314717f7c14d41a119bbfa
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-05-27 17:03:53 +0100

    correct ncbiTax-genbanks relationship

--- cazy_webscraper/sql/sql_orm.py
+++ cazy_webscraper/sql/sql_orm.py
@@ -193,11 +193,7 @@ class Genbank(Base):
     sequence = Column(ReString)
     seq_update_date = Column(ReString)
     taxonomy_id = Column(Integer, ForeignKey("Taxs.taxonomy_id"))
-    
-    ncbi_taxs = relationship(
-        "NcbiTax",
-        back_populates="genbanks",
-    )
+    ncbi_tax_id = Column(Integer, ForeignKey("NcbiTaxs.ncbi_tax_id"))
 
     organism = relationship(
         "Taxonomy",
@@ -326,13 +322,14 @@ class NcbiTax(Base):
     # define columns before table_args so subfam column can be called
     ncbi_id = Column(Integer, primary_key=True)
     ncbi_tax_id = Column(Integer)  # make this an ReString later
-    genus = Column(ReString)
-    species = Column(ReString)
     kingdom = Column(ReString)
     phylum = Column(ReString)
     tax_class = Column(ReString)
     order = Column(ReString)
     family = Column(ReString)
+    genus = Column(ReString)
+    species = Column(ReString)
+    strain = Column(ReString)
     
     __table_args__ = (
         UniqueConstraint("ncbi_tax_id"),
