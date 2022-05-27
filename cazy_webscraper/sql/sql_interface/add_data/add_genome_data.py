cf190fbc2412402695eb7410dbcf712bd10a3043
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-05-27 09:56:27 +0100

    update protein records with genome ids

--- cazy_webscraper/sql/sql_interface/add_data/add_genome_data.py
+++ cazy_webscraper/sql/sql_interface/add_data/add_genome_data.py
@@ -47,14 +47,50 @@ from sqlalchemy import text
 from tqdm import tqdm
 
 from cazy_webscraper.sql.sql_interface import insert_data
+from cazy_webscraper.sql.sql_interface.get_data.get_assemblies import get_records_to_update, get_assembly_table
 
 
-def add_genomic_data(genome_dict, assembly_dict, gbk_dict, connection):
+def add_assembly_data(assembly_dict, genome_dict, gbk_dict, connection, args):
+    """Coordinate adding genomic data and associting proteins with genomes in the local database
+    
+    :param assembly_dict: dict, {assembly_name: {protein_accessions}}
+    :param genome_dict: dict, listing assembly meta data
+    :param gbk_dict: dict, {gbk_accession: local db id}
+    :param connection: open sqlite db connection
+    :param args: cmd-line args parser
+    
+    Return nothing
+    """
+    logger = logging.getLogger(__name__)
+
+    if args.update:
+        add_genomic_data(genome_dict, connection)
+
+    else:
+        update_genomic_data(genome_dict, connection)
+
+    # load assembly table {ass_name: db id}
+    db_genome_table_dict = get_assembly_table(genome_dict, connection)
+
+    # identify protein records to update
+    gbk_genome_dict = {}  # assembly db id: protein db id
+    
+    for assembly_name in tqdm(assembly_dict, desc="Identifying protein records to update"):
+        assembly_proteins = assembly_dict[assembly_name]
+
+        for protein in assembly_proteins:
+            protein_id = gbk_dict[protein]
+            assembly_id = db_genome_table_dict[assembly_name]
+            gbk_genome_dict[protein_id] = assembly_id
+
+    # update protein records
+
+
+
+def add_genomic_data(genome_dict, connection):
     """Add new genomic assembly data to the local CAZyme database
     
     :param genome_dict: dict, listing assembly meta data
-    :param assembly_dict: dict, {assembly_name: {protein_accessions}}
-    :param gbk_dict: dict, {gbk_accession: local db id}
     :param connection: open sqlite db connection
 
     Return nothing
@@ -63,21 +99,19 @@ def add_genomic_data(genome_dict, assembly_dict, gbk_dict, connection):
 
     records_to_add = set()
 
-    for assembly_name in tqdm(assembly_dict, desc="Compiling data to add to db"):
-        assembly_proteins = assembly_dict[assembly_name]
+    for assembly_name in tqdm(genome_dict, desc="Compiling data to add to db"):
         gbk_ver_acc = genome_dict[assembly_name]['gbk_acc']
         gbk_ncbi_id = genome_dict[assembly_name]['gbk_uid']
         ref_ver_acc = genome_dict[assembly_name]['refseq_acc']
         ref_ncbi_id = genome_dict[assembly_name]['refseq_uid']
-        for protein_acc in assembly_proteins:
-            db_id = gbk_dict[protein_acc]
-            records_to_add.add((db_id, assembly_name, gbk_ver_acc, gbk_ncbi_id, ref_ver_acc, ref_ncbi_id))
+        
+        records_to_add.add((assembly_name, gbk_ver_acc, gbk_ncbi_id, ref_ver_acc, ref_ncbi_id))
 
     if len(records_to_add) != 0:
         insert_data(
             connection,
             'Genomes',
-            ['genbank_id', 'assembly_name', 'gbk_version_accession', 'gbk_ncbi_id', 'refseq_version_accession', 'refseq_ncbi_id'],
+            ['assembly_name', 'gbk_version_accession', 'gbk_ncbi_id', 'refseq_version_accession', 'refseq_ncbi_id'],
             list(records_to_add),
         )
     
@@ -122,4 +156,27 @@ def update_genomic_data(update_gbk_dict, assembly_dict, genome_dict, connection)
                     )
                 )
     
+    return
+
+
+def update_protein_genomic_data(gbk_genome_dict, connection):
+    """Update the genomic assembly data in the protein (gbk) table
+    
+    :param gbk_genome_dict: dict, db protein id: assembl db id
+    :param conneciton: open sql db connection
+    
+    Return nothing
+    """
+    for protein_id in tqdm(gbk_genome_dict, desc="Updating db protein records"):
+        assembly_id = gbk_genome_dict[protein_id]
+
+        with connection.begin():
+            connection.execute(
+                text(
+                    "UPDATE Genbanks "
+                    f"SET genome_id = {assembly_id} "
+                    f"WHERE genbank_id = '{protein_id}'"
+                )
+            )
+
     return
