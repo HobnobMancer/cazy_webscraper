c25d45bb3f8b09a9d5a963388a3f2e1c10cc3e5e
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-05-28 11:33:24 +0100

    define rel between Genbanks and NcbiTaxs

--- cazy_webscraper/sql/sql_orm.py
+++ cazy_webscraper/sql/sql_orm.py
@@ -195,6 +195,11 @@ class Genbank(Base):
     taxonomy_id = Column(Integer, ForeignKey("Taxs.taxonomy_id"))
     ncbi_id = Column(Integer, ForeignKey("NcbiTaxs.ncbi_id"))
 
+    ncbi_taxs = relationship(
+        "NcbiTax",
+        back_populates="genbanks",
+    )
+
     organism = relationship(
         "Taxonomy",
         back_populates="genbanks",
