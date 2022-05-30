dcec3a2cc286de7aa6d2289f26f57e2ca45be1aa
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-05-30 08:58:10 +0100

    add common post_ids func

---
+++ cazy_webscraper/ncbi/__init__.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) University of St Andrews 2020-2021
+# (c) University of Strathclyde 2020-2021
+# Author:
+# Emma E. M. Hobbs
+
+# Contact
+# eemh1@st-andrews.ac.uk
+
+# Emma E. M. Hobbs,
+# Biomolecular Sciences Building,
+# University of St Andrews,
+# North Haugh Campus,
+# St Andrews,
+# KY16 9ST
+# Scotland,
+# UK
+
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""Module for interacting with the NCBI databases."""
+
+
+import logging
+
+from Bio import Entrez
+from saintBioutils.genbank import entrez_retry
+
+
+def post_ids(ids, database, args):
+    """Post protein IDs to Entrez
+    
+    :param ids: list, GenBank protein accession numbers
+    :param database: str, Name of database from which IDs are sourced
+    :param args: cmd-line args parser
+    
+    Return None (x2) if fails
+    Else return query_key and web_env
+    """
+    logger = logging.getLogger(__name__)
+
+    try:
+        with entrez_retry(
+            args.retries,
+            Entrez.epost,
+            db=database,
+            id=",".join(ids),
+        ) as handle:
+            posted_records = Entrez.read(handle, validate=False)
+
+    # if no record is returned from call to Entrez
+    except (TypeError, AttributeError) as err:
+        logger.warning(
+            f"Failed to post IDs to Entrez {database} db:\n{err}"
+        )
+        return None, None
+
+    query_key = posted_records['QueryKey']
+    web_env = posted_records['WebEnv']
+
+    return query_key, web_env
