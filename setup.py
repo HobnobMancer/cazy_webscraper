7621a8926b5e57492a57abaacc8bf9cf9ab74125
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-08-11 17:25:35 +0100

    add gtdb tax retrieval entry point

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setuptools.setup(
             "cw_get_genbank_seqs = cazy_webscraper.expand.genbank.sequences.get_genbank_sequences:main",
             "cw_get_ncbi_taxs = cazy_webscraper.expand.genbank.taxonomy.get_ncbi_taxs:main",
             "cw_get_genomics = cazy_webscraper.expand.genbank.genomes.get_genome_accs:main",
+            "cw_get_gtdb_taxs = cazy_webscraper.expand.gtdb.get_gtdb_tax:main",
             "cw_get_uniprot_data = cazy_webscraper.expand.uniprot.get_uniprot_data:main",
             "cw_extract_db_seqs = cazy_webscraper.expand.extract_seqs.extract_db_seqs:main",
             "cw_get_pdb_structures = cazy_webscraper.expand.pdb.get_pdb_structures:main",
