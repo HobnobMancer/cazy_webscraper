722f87327f428f5f533a69b3fdae9b5353156f63
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-08-11 17:15:22 +0100

    use user provided GTDB datafiles

--- cazy_webscraper/expand/gtdb/__init__.py
+++ cazy_webscraper/expand/gtdb/__init__.py
@@ -64,12 +64,12 @@ GTDB_URL = "https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/"
 
 def get_gtdb_data(args, cache_dir, arch, bact):
     """Download taxonomic data files from the GTDB website
-    
+
     :param args: cmd-line args parser
     :param cache_dir: path to cache directory
     :param arch: bool, whether to download arch datafile
     :param bact: bool, whether to download the bact datafile
-    
+
     Return paths to downloaded archea and bacteria data files. None if kingdom not selected for DL
     """
     logger = logging.getLogger(__name__)
@@ -85,63 +85,71 @@ def get_gtdb_data(args, cache_dir, arch, bact):
 
     archaea_link, bacteria_link = None, None
 
-    for i in gtdb_release_page.select("table")[0].select("tr"):
-        for j in i.select("td"):
-            if j.contents[0] is not None:
-                try:
-                    if j.contents[0]['href'].endswith('_taxonomy.tsv.gz'):
-                        if j.contents[0]['href'].startswith('ar'):
-                            archaea_link = f"{GTDB_URL}{j.contents[0]['href']}"
-                        else:
-                            bacteria_link = f"{GTDB_URL}{j.contents[0]['href']}"
-                        print(j.contents[0]['href'])
-                except (KeyError, TypeError):
-                    continue
-    
-    if archaea_link is None or bacteria_link is None:
-        if archaea_link is None and arch:
-            logger.error(
-                "Failed to get archeae GTDB data release page\n"
-                "Retrieved datafile urls:\n"
-                f"Archaea: {archaea_link}"
-            )
-        if bacteria_link is None and bact:
-            logger.error(
-                "Failed to get bacteria GTDB data release page\n"
-                "Retrieved datafile urls:\n"
-                f"Archaea: {bacteria_link}"
-            )
-        logger.error("Failed to retrieve download URLs\nTerminating program")
-        sys.exit(1)
+    if args.archaea_file is not None or args.bacteria_file is not None:
+        # need to retrieve links from the website
+        for i in gtdb_release_page.select("table")[0].select("tr"):
+            for j in i.select("td"):
+                if j.contents[0] is not None:
+                    try:
+                        if j.contents[0]['href'].endswith('_taxonomy.tsv.gz'):
+                            if j.contents[0]['href'].startswith('ar'):
+                                archaea_link = f"{GTDB_URL}{j.contents[0]['href']}"
+                            else:
+                                bacteria_link = f"{GTDB_URL}{j.contents[0]['href']}"
+                            print(j.contents[0]['href'])
+                    except (KeyError, TypeError):
+                        continue
+
+        if archaea_link is None or bacteria_link is None:
+            if archaea_link is None and arch:
+                logger.error(
+                    "Failed to get archeae GTDB data release page\n"
+                    "Retrieved datafile urls:\n"
+                    f"Archaea: {archaea_link}"
+                )
+            if bacteria_link is None and bact:
+                logger.error(
+                    "Failed to get bacteria GTDB data release page\n"
+                    "Retrieved datafile urls:\n"
+                    f"Archaea: {bacteria_link}"
+                )
+            logger.error("Failed to retrieve download URLs\nTerminating program")
+            sys.exit(1)
 
     archaea_file, bacteria_file = None, None
 
     if arch:
-        arch_release = archaea_link.split("/")[-1].split("_")[0]
-        archaea_file = cache_dir / f"archaea_data-{arch_release}.gz"
+        if args.archaea_file is not None:
+            archaea_file = args.archaea_file
+        else:
+            arch_release = archaea_link.split("/")[-1].split("_")[0]
+            archaea_file = cache_dir / f"archaea_data-{arch_release}.gz"
 
-        downloaded = download_gtdb_data(archaea_link, archaea_file, 'Archaea')
+            downloaded = download_gtdb_data(archaea_link, archaea_file, 'Archaea')
 
-        if downloaded is False:
-            logger.error("Failed to download archaea GTDB data file\nTerminating program")
-            sys.exit(1)
-    
+            if downloaded is False:
+                logger.error("Failed to download archaea GTDB data file\nTerminating program")
+                sys.exit(1)
+
     if bact:
-        bact_release = bacteria_link.split("/")[-1].split("_")[0]
-        bacteria_file = cache_dir / f"bacteria_data-{bact_release}.gz"
+        if args.bacteria_file is not None:
+            bacteria_file = args.bacteria_file
+        else:
+            bact_release = bacteria_link.split("/")[-1].split("_")[0]
+            bacteria_file = cache_dir / f"bacteria_data-{bact_release}.gz"
 
-        downloaded = download_gtdb_data(bacteria_link, bacteria_file, 'Bacteria')
+            downloaded = download_gtdb_data(bacteria_link, bacteria_file, 'Bacteria')
 
-        if downloaded is False:
-            logger.error("Failed to download bacteria GTDB data file\nTerminating program")
-            sys.exit(1)
-    
+            if downloaded is False:
+                logger.error("Failed to download bacteria GTDB data file\nTerminating program")
+                sys.exit(1)
+
     return archaea_file, bacteria_file
 
 
 def download_gtdb_data(url, out_file_path, gtdb_group):
     logger = logging.getLogger(__name__)
-    
+
     # Try URL connection
     try:
         response = urlopen(url, timeout=45)
@@ -152,10 +160,10 @@ def download_gtdb_data(url, out_file_path, gtdb_group):
         return False
 
     logger.info(f"Downloading data file from:\n{url}")
-    
+
     file_size = int(response.info().get("Content-length"))
     bsize = 1_048_576
-    
+
     try:
         with open(out_file_path, "wb") as out_handle:
             with tqdm(
