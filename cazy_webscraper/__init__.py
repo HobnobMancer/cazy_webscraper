a563b6c2afa1e7337e8f228e972dff94d1b28f48
HobnobMancer <eemh1@st-andrews.ac.uk>
2022-12-08 15:30:16 +0000

    update version number

--- cazy_webscraper/__init__.py
+++ cazy_webscraper/__init__.py
@@ -54,7 +54,7 @@ from saintBioutils.utilities.file_io import make_output_directory
 from cazy_webscraper.sql import sql_orm
 
 
-__version__ = "2.2.4"
+__version__ = "2.2.3"
 
 
 VERSION_INFO = f"cazy_webscraper version: {__version__}"
