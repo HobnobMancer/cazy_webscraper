2dd530ee20d80adbe1ed10c30b83c88531a8028d
HobnobMancer <hobnobmancer@Paddington.localdomain>
2023-04-26 13:50:40 +0100

    tolerate adding parent dirs during unit tests

--- tests/test_sql_ad_genbank.py
+++ tests/test_sql_ad_genbank.py
@@ -102,7 +102,7 @@ def test_add_gbk_seqs(monkeypatch, db_path):
     monkeypatch.setattr(add_genbank_data, "get_gbk_table_seq_dict", mock_gbk_table_seq_dict)
 
     cache_dir = Path("tests/test_outputs/test_outputs_sql/temp_dir")
-    cache_dir.mkdir(exist_ok=True)
+    cache_dir.mkdir(exist_ok=True, parents=True)
 
     add_genbank_data.add_gbk_seqs_to_db(
         seq_dict,
